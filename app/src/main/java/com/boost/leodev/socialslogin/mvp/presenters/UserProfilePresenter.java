da01980190a5894fea6eef74fd53e4c9be78b6ad
leonid <v666ad2009@gmail.com>
2017-04-12 18:48:43 +0300

    Show user info. Implement logout

--- app/src/main/java/com/boost/leodev/socialslogin/mvp/presenters/UserProfilePresenter.java
+++ app/src/main/java/com/boost/leodev/socialslogin/mvp/presenters/UserProfilePresenter.java
@@ -2,9 +2,29 @@ package com.boost.leodev.socialslogin.mvp.presenters;
 
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
+import com.boost.leodev.socialslogin.MyApplication;
+import com.boost.leodev.socialslogin.event.EventMainChangeFragment;
 import com.boost.leodev.socialslogin.mvp.views.UserProfileView;
+import com.boost.leodev.socialslogin.ui.fragments.LoginFragment;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.greenrobot.eventbus.EventBus;
 
 
 @InjectViewState
 public class UserProfilePresenter extends MvpPresenter<UserProfileView> {
+
+    public void logOut() {
+        LoginManager.getInstance().logOut();
+
+        GoogleApiClient googleApiClient = MyApplication.getGoogleApiClient();
+        if (googleApiClient.isConnected())
+            Auth.GoogleSignInApi.signOut(googleApiClient);
+
+        EventBus.getDefault().post(new EventMainChangeFragment(LoginFragment.newInstance()));
+    }
+
+
 }
--- app/src/main/java/com/boost/leodev/socialslogin/ui/fragments/UserProfileFragment.java
+++ app/src/main/java/com/boost/leodev/socialslogin/ui/fragments/UserProfileFragment.java
@@ -1,7 +1,9 @@
 package com.boost.leodev.socialslogin.ui.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +16,11 @@ import com.boost.leodev.socialslogin.R;
 import com.boost.leodev.socialslogin.mvp.models.User;
 import com.boost.leodev.socialslogin.mvp.presenters.UserProfilePresenter;
 import com.boost.leodev.socialslogin.mvp.views.UserProfileView;
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
@@ -35,6 +39,11 @@ public class UserProfileFragment extends MvpAppCompatFragment implements UserPro
     @BindView(R.id.btn_out)
     ImageButton mLogout;
 
+    @OnClick(R.id.btn_out)
+    public void onClick(){
+        mPresenter.logOut();
+    }
+
     private static final String TAG = "UserProfileFragment";
     private static final int LAYOUT = R.layout.fragment_user_profile;
     private static final String ARGS_USER = "ARGS_USER";
@@ -52,6 +61,18 @@ public class UserProfileFragment extends MvpAppCompatFragment implements UserPro
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(LAYOUT, container, false);
         ButterKnife.bind(this, view);
+
+        User user = getArguments().getParcelable(ARGS_USER);
+
+        if (user != null) {
+            mUserName.setText(user.getName());
+            mUserEmail.setText(user.getEmail());
+            mBirthDay.setText(user.getBirthDay());
+            Log.d(TAG, "onCreateView: " + user.getPhotoUri());
+            Glide.with(this)
+                    .load(Uri.parse(user.getPhotoUri()))
+                    .into(mProfileIcon);
+        }
 
         return view;
     }
